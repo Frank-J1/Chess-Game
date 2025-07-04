a8feab297493193fab3b0433b27b6884910cc584
Frank-J1 <frankmartinscnd@gmail.com>
2025-07-04 13:09:45 -0400

    add turn changing and move validation

--- src/main.py
+++ src/main.py
@@ -8,5 +8,20 @@ def main():
     coords = board.parse_coordinates(move_string)
     print(coords)
 
+    current_turn = "white"
+
+    while True:
+        move_string = input("Enter your move (e.g., 'e2 e4'): ")
+        coords = board.parse_coordinates(move_string)
+
+        if coords is None:
+            print ("please enter a valid move.!")
+            continue 
+        
+        start_pos, end_pos = coords
+
+        if board.move_piece(start_pos, end_pos, current_turn):
+            current_turn = "black" if current_turn == "white" else "white"
+
 if __name__ == "__main__":
     main()
